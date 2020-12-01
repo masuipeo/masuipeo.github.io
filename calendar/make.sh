58b42be513249be959215abcecac4d5aa9d53611
masuipeo <info@masuipeo.com>
2020-12-01 09:10:52 +0900

    2021

--- calendar/make.sh
+++ calendar/make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for i in {2010..2020} ; do
+#for i in {2010..2020} ; do
+for i in {2021..2021} ; do
     for j in {1..12} ; do
         php calendar.php ${i} ${j} > ${i}$(printf "%02d" $j).png
     done
